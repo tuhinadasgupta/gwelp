ab414c662086e11c42d659a5293fd392c7c49444
Melody Lee <melodylee299@gmail.com>
2019-04-05 14:04:47 -0400

    Enable/disable login button

--- app/src/main/java/edu/gwu/gwelp/MainActivity.kt
+++ app/src/main/java/edu/gwu/gwelp/MainActivity.kt
@@ -2,11 +2,47 @@ package edu.gwu.gwelp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var username: EditText
+    private lateinit var password: EditText
+    private lateinit var login: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    // Anonymous class implementing TextWatcher
+    private val textWatcher: TextWatcher = object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {}
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            val inputtedUsername: String = username.text.toString().trim()
+            val inputtedPassword: String = password.text.toString().trim()
+            val enableButton: Boolean = inputtedUsername.isNotEmpty() && inputtedPassword.isNotEmpty()
+
+            login.isEnabled = enableButton
+        }
+
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        login = findViewById(R.id.login)
+        progressBar = findViewById(R.id.progressBar)
+
+        username.addTextChangedListener(textWatcher)
+        password.addTextChangedListener(textWatcher)
     }
 }
