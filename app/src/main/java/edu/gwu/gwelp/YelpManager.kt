46fc2b4cd1e18af59bee79c3e66bb42d6664ac68
Melody Lee <melodylee299@gmail.com>
2019-04-20 21:56:13 -0400

    Yelp api business search endpoint call

---
+++ app/src/main/java/edu/gwu/gwelp/YelpManager.kt
@@ -0,0 +1,94 @@
+package edu.gwu.gwelp
+
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
+import java.io.IOException
+import java.util.concurrent.TimeUnit
+
+class YelpManager {
+
+    private val okHttpClient: OkHttpClient
+
+    init {
+        val builder = OkHttpClient.Builder()
+
+        // This sets network timeouts (in case the phone can't connect
+        // to the server or the server is down)
+        builder.connectTimeout(20, TimeUnit.SECONDS)
+        builder.readTimeout(20, TimeUnit.SECONDS)
+        builder.writeTimeout(20, TimeUnit.SECONDS)
+
+        // This causes all network traffic to be logged to the console
+        val logging = HttpLoggingInterceptor()
+        logging.level = HttpLoggingInterceptor.Level.BODY
+        builder.addInterceptor(logging)
+
+        okHttpClient = builder.build()
+    }
+
+    // Get Yelp businesses within 1.25 miles of given address string, sorted by distance
+    fun retrieveBusinesses(
+        apiKey: String,
+        address: String, // Name of the landmark
+        successCallback: (List<Business>) -> Unit,
+        errorCallback: (Exception) -> Unit
+    ) {
+
+        // Build request URL with query parameters
+        // https://api.yelp.com/v3/businesses/search
+        val url = HttpUrl.Builder()
+            .scheme("https")
+            .host("api.yelp.com")
+            .addPathSegments("v3/businesses.search")
+            .addQueryParameter("location", address)
+            .addQueryParameter("radius", "2000") // 2000 meters approx 1.25 miles
+            .addQueryParameter("sort_by", "distance")
+            .build()
+
+        // Building the request, passing the api key as a header
+        val request = Request.Builder()
+            .url(url)
+            .header("Authorization", "Bearer $apiKey")
+            .build()
+
+        okHttpClient.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                errorCallback(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val businesses = mutableListOf<Business>()
+                val responseString = response.body()?.string()
+
+                if (response.isSuccessful && responseString != null) {
+                    val results = JSONObject(responseString).getJSONArray("businesses")
+                    for (i in 0 until results.length()) {
+                        val curr = results.getJSONObject(i)
+                        val name = curr.getString("name")
+                        val id = curr.getString("id")
+                        val location = curr.getJSONObject("location")
+                        val address1 = location.getString("address1")
+                        val coordinates = curr.getJSONObject("coordinates")
+                        val latitude = coordinates.getDouble("latitude")
+                        val longitude = coordinates.getDouble("longitude")
+                        businesses.add(
+                            Business(
+                                name = name,
+                                id = id,
+                                address = address1,
+                                lat = latitude,
+                                lon = longitude
+                            )
+                        )
+                    }
+                    successCallback(businesses)
+                    //...
+                } else {
+                    // Invoke the callback passed to our [retrieveBusinesses] function.
+                    errorCallback(Exception("Business Search call failed"))
+                }
+            }
+        })
+    }
+}
