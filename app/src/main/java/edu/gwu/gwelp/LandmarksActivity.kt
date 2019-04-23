1188eafa326950f0cf58a34012021ddcecdd125a
Tuhina Dasgupta <tuhinad@gwu.edu>
2019-04-23 17:54:33 -0400

    spinner working

--- app/src/main/java/edu/gwu/gwelp/LandmarksActivity.kt
+++ app/src/main/java/edu/gwu/gwelp/LandmarksActivity.kt
@@ -2,10 +2,13 @@ package edu.gwu.gwelp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
+import android.widget.Toast
 
-class LandmarksActivity: AppCompatActivity()  {
+class LandmarksActivity: AppCompatActivity(), AdapterView.OnItemSelectedListener  {
     private lateinit var spinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +26,31 @@ class LandmarksActivity: AppCompatActivity()  {
             // Apply the adapter to the spinner
             spinner.adapter = adapter
         }
+
+        spinner.onItemSelectedListener = this
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+        // An item was selected. You can retrieve the selected item using
+        val selected : String
+        if (pos != 0) {
+             selected = parent.getItemAtPosition(pos) as String
+            Toast.makeText(
+                this,
+                "$selected was selected!",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+        //new intent page to be opened
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback
+        Toast.makeText(
+            this,
+            "Please select one of the landmarks!",
+            Toast.LENGTH_LONG
+        ).show()
 
     }
 
