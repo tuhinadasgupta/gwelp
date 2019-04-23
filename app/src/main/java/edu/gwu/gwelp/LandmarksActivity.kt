1120bbec13e27818a77150d7ae23a3d989eca0d5
Melody Lee <melodylee299@gmail.com>
2019-04-23 17:03:50 -0400

    Function to compare yelp response to gworld.json

--- app/src/main/java/edu/gwu/gwelp/LandmarksActivity.kt
+++ app/src/main/java/edu/gwu/gwelp/LandmarksActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import org.json.JSONArray
+import java.io.File
 
 class LandmarksActivity: AppCompatActivity()  {
     companion object {
@@ -35,6 +37,38 @@ class LandmarksActivity: AppCompatActivity()  {
 
         // Create the adapter and assign it to the RecyclerView
         recyclerView.adapter = LandmarksAdapter(LANDMARK_NAMES)
+
+    }
+
+    fun findGworldRestaurants(yelpResponse: List<Business>) {
+        val gworldString: String = File("/gworld.json").readText(Charsets.UTF_8)
+        val jsonArray = JSONArray(gworldString)
+        yelpResponse.forEach  { yelpBusiness->
+            for (i in 0 until jsonArray.length()) {
+                val curr = jsonArray.getJSONObject(i)
+                val gworldName = curr.getString("name")
+                val gworldLat = curr.getDouble("lat")
+                val gworldLon = curr.getDouble("lon")
+
+                if (
+                    yelpBusiness.lat > gworldLat - 1
+                    && yelpBusiness.lat < gworldLat + 1
+                    && yelpBusiness.lon > gworldLon - 1
+                    && yelpBusiness.lon < gworldLon + 1
+                    && yelpBusiness.name == gworldName
+                ) {
+                    // Yelp Business Reviews API call using yelpBusiness.id
+                    // Go to next activity displaying review excerpts
+//                    val yelpManager = YelpManager()
+//                    yelpManager.retrieveReviews(
+//                        apiKey = getString(R.string.yelp_api_key),
+//                        businessId = yelpBusiness.id,
+//                        successCallback = {},
+//                        errorCallback = {}
+//                    )
+                }
+            }
+        }
 
     }
 
